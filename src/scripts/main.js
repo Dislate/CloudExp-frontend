9f6bdebb530ad6e8735e27f096991ec5ad05bc4c
Cyrus <wiranenar@gmail.com>
2021-01-31 20:36:52 +0300

    index page added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
-console.log('Test');
+function sideMenu() {
+    const hamburger = document.querySelectorAll('.hamburger');
+    const mobileNav = document.querySelector('.mobile-nav');
+    console.log(hamburger)
+    hamburger.forEach((element) => {
+        element.addEventListener('click', (event) => {
+            mobileNav.classList.toggle('grid');
+            mobileNav.classList.toggle('none');
+        })
+    })
+}
+
+sideMenu();
